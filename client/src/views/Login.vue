8c0f4ef19d9e3180d2683e1f7b20ebe134837fa0
oooo0000oooo <35052074+kmk5010@users.noreply.github.com>
2018-12-15 22:16:53 -0500

    added new ticket page and tickets service

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -45,7 +45,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container-fluid {
   margin-right: auto;
   margin-left: auto;
--- client/src/views/NewTicket.vue
+++ client/src/views/NewTicket.vue
@@ -1,13 +1,88 @@
 <template>
-  <h1>this is a new ticket page</h1>
+  <div class="container-fluid">
+    <b-card class="mt-4 rounded-10" title="Customer Information">
+      <b-container>
+        <b-form-group>
+          <b-row class="mt-2">
+            <b-col>
+              <b-form-input id="firstName" type="text" placeholder="First Name" v-model="firstName"></b-form-input>
+            </b-col>
+            <b-col>
+              <b-form-input id="lastName" type="text" placeholder="Last Name" v-model="lastName"></b-form-input>
+            </b-col>
+          </b-row>
+          <b-row class="mt-4">
+            <b-col>
+              <b-form-input id="phoneNumber" type="number" placeholder="Phone Number" v-model="phone"></b-form-input>
+            </b-col>
+            <b-col>
+              <b-form-input id="email" type="email" placeholder="Email" v-model="email"></b-form-input>
+            </b-col>
+          </b-row>
+        </b-form-group>
+      </b-container>
+    </b-card>
+      <b-card class="mt-4 rounded-10" title="Computer Information">
+      <b-container>
+        <b-form-group>
+          <b-row class="mt-2">
+            <b-col>
+              <b-form-input id="model" type="text" placeholder="Model" v-model="model"></b-form-input>
+            </b-col>
+            <b-col>
+              <b-form-input id="serial" type="text" placeholder="Serial Number" v-model="serial"></b-form-input>
+            </b-col>
+          </b-row>
+          <b-row class="mt-4">
+            <b-col>
+              <b-form-textarea id="issue" type="textarea" placeholder="Issue Description" :rows="3" v-model="issue"></b-form-textarea>
+            </b-col>
+          </b-row>
+          <b-button class="m-2" @click="submitTicket" variant="dark">Submit</b-button>
+        </b-form-group>
+      </b-container>
+    </b-card>
+  </div>
 </template>
 
 <script>
+import api from '@/api'
 export default {
-  name: 'NewTicket'
+  name: 'NewTicket',
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      email: '',
+      model: '',
+      serial: '',
+      issue: ''
+    }
+  },
+  methods: {
+    submitTicket () {
+      api.authenticate()
+      api.service('tickets').create({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        phone: this.phone,
+        email: this.email,
+        model: this.model,
+        serial: this.serial,
+        issue: this.issue,
+        status: 'queued'
+      })
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.container-fluid {
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 800px;
+}
 </style>
